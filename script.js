3cd57c8ad5452c31b13a29f0b280d6758e1f7f2e
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-07-04 21:55:49 -0400

    Add dark mode

--- script.js
+++ script.js
@@ -144,4 +144,22 @@ document.addEventListener("DOMContentLoaded", () => {
       forecastContainer.appendChild(forecastElement);
     });
   }
+  document
+    .getElementById("toggle-dark-mode")
+    .addEventListener("click", function () {
+      document.body.classList.toggle("dark-mode");
+      // Save user preference
+      if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+    });
+
+  // Load user preference on page load
+  window.addEventListener("load", function () {
+    if (localStorage.getItem("theme") === "dark") {
+      document.body.classList.add("dark-mode");
+    }
+  });
 });
